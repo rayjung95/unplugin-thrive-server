f1a9638cac336451c2320a9f89931a9da09d7f39
rayjung95 <rayjung95@gmail.com>
2019-02-10 21:09:41 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# unplugin-thrive-server
---
+++ api/routes/orders.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Orders were fetched'
+    });
+});
+
+
+router.post('/', (req, res, next) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    }
+    res.status(201).json({
+        message: 'Order was created',
+        order: order
+    });
+});
+
+router.get('/:orderId', (req, res, next) => {
+    const id = req.params.orderId;
+    res.status(200).json({
+        message: '',
+        id: id
+    })
+
+});
+
+router.delete('/:orderId', (req, res, next) => {
+    const id = req.params.orderId;
+    res.status(200).json({
+        message: 'order deleted',
+        id: id
+    })
+});
+
+module.exports = router;
+
